204fb1bb74935bf9487ad2b9ca203fa2a8a164e8
Chris Rudzki <chris@rudzki.org>
2017-11-30 12:44:35 -0500

    style

--- extract-links-from-twitter-timeline.py
+++ extract-links-from-twitter-timeline.py
@@ -9,19 +9,19 @@ from creds import TWITTER_KEY, TWITTER_KEY_SECRET, TWITTER_TOKEN, TWITTER_TOKEN_
 # credentials
 
 config = {
-    "consumer_key" : TWITTER_KEY,
-    "consumer_secret" : TWITTER_KEY_SECRET,
-    "access_key" : TWITTER_TOKEN,
-    "access_secret" : TWITTER_TOKEN_SECRET
+    "consumer_key": TWITTER_KEY,
+    "consumer_secret": TWITTER_KEY_SECRET,
+    "access_key": TWITTER_TOKEN,
+    "access_secret": TWITTER_TOKEN_SECRET
 }
 
 # twitter api object
 
 twitter = Twitter(
-    auth = OAuth(config["access_key"],
-    config["access_secret"],
-    config["consumer_key"],
-    config["consumer_secret"]),
+    auth=OAuth(config["access_key"],
+               config["access_secret"],
+               config["consumer_key"],
+               config["consumer_secret"]),
     secure=True)
 results = twitter.statuses.home_timeline()
 
@@ -31,18 +31,19 @@ results = twitter.statuses.home_timeline()
 
 for tweet in results:
     for url in tweet["entities"]["urls"]:
-        if ( "https://twitter.com" not in url["expanded_url"] ):
+        if ("https://twitter.com" not in url["expanded_url"]):
             tweet_username = tweet["user"]["screen_name"]
             tweet_url = re.sub('\?.*', '', url["expanded_url"])
             browser = mechanicalsoup.StatefulBrowser()
             browser.open(tweet_url)
             page = browser.get_current_page()
             tweet_title = page.title.text
-            print(tweet_title + " " + "[@" + tweet_username + "]",
-                  "\n", tweet_url, "\n")
+            print(tweet_title,
+                  "[Shared by @" + tweet_username + "]",
+                  "\n", tweet_url, "\n"
+                  )
 
 
 # todo: returned tweets seem few; probably hitting limit
 # todo: store in database for X days
 # todo: web interface using flask
-
